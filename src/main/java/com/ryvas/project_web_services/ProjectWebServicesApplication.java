74d8df44a4051151bd784721ab4c19f9778fa3ee
RyanVandir <r.vandir@outlook.com>
2026-01-20 16:41:07 -0300

    Criação estrutura de projeto

---
+++ src/main/java/com/ryvas/project_web_services/ProjectWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ryvas.project_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebServicesApplication.class, args);
+	}
+
+}
